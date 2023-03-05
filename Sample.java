60e6a82ccb1ea6740c821614da97bb77b515e5cd
testuser <testuser@gmail.com>
2023-03-05 12:55:57 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sample
+{
+public static void main(String args[])
+{
+String name ="vamsi";
+int l=name.length();
+System.out.println(l);
+// /*
+// for (int i=0;i<l;i++)
+// {
+//     String arr[] = name.split("");
+//     int new[] = (int) arr[];
+//     new=new+1;
+//     System.out.println(new);
+//     //System.out.println(arr[i]);
+// }
+// System.out.println();
+// */
+for (int i = 0; i<l; i = i+2){
+    System.out.println(i);
+}
+}
+}
