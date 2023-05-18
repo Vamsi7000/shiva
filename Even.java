3db3a062b4691fe02cdd3aa43d539fb638e9289a
vk <testuser@gmail.com>
2023-05-18 21:21:01 +0530

    first change

---
+++ Even.java
@@ -0,0 +1,25 @@
+	//even or odd
+
+import java.util.*;
+import java.util.Scanner;
+class Even
+{
+public static void main(String args[])
+{
+
+int j;
+for (j=1;j<=10;j++)
+{
+if ( j % 2 ==0)
+{
+System.out.println("Even number: "+j);
+
+}
+else
+{ 
+System.out.println("odd number: " +j);
+
+}
+}
+}
+}
